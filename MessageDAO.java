a782426535412a88a7b3fe192eb5abfa31f29448
vlakd1059 <49181149+vlakd1059@users.noreply.github.com>
2022-01-14 14:23:26 +0900

    Add files via upload

---
+++ MessageDAO.java
@@ -0,0 +1,54 @@
+package com.message.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MessageDAO {
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+	private int cnt;
+	private String sql;
+
+	public void connect() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "hr";
+			String password = "hr";
+
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			// 1. OracleDriver 클래스가 해당 위치에 없는경우(ojdbc6.jar 미포함)
+			// 해결방안 : WEB-INF -> lib -> ojdbc6.jar 저장
+
+			// 2. OracleDriver 경로가 오타인경우
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// DB연결 정보가 정확하지 않을 경우
+			e.printStackTrace();
+		}
+	}
+
+	// DB연결종료 -> 연결 종류 순서는 역순으로
+	public void close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
